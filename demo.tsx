583e1b7ca31ad18fe0f992dd909f769825ac72ab
ppozzobon <59428040+ppozzobon@users.noreply.github.com>
2022-12-29 17:40:56 -0300

    2 files modified

--- demo.tsx
+++ demo.tsx
@@ -3,6 +3,13 @@ import './index.css';
 import { Select } from 'antd';
 import type { SelectProps } from 'antd';
 
+import type { DatePickerProps } from 'antd';
+import { DatePicker, Space } from 'antd';
+
+const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+  console.log(date, dateString);
+};
+
 const clientes: SelectProps['options'] = [
   {value: "ATUAL CARGAS TRANSPORTES LTDA - 08848231000161", label: "ATUAL CARGAS TRANSPORTES LTDA - 08848231000161"},
   {value: "TLX TRANSPORTE E LOGISTICA LTDA - 15475228000296", label: "TLX TRANSPORTE E LOGISTICA LTDA - 15475228000296"},
@@ -13,6 +20,24 @@ const fornecedores: SelectProps['options'] = [
   {value: "REDE DE POSTOS MARAJO HIDROLANDIA LTDA - 42354394000126", label: "REDE DE POSTOS MARAJO HIDROLANDIA LTDA - 42354394000126"},
   {value: "REDE DE POSTOS MARAJO CENTRALINA LTDA - 24187119000183", label: "REDE DE POSTOS MARAJO CENTRALINA LTDA - 24187119000183"},
   {value: "POSTO AGRICOPEL LTDA. - 83488882000456", label: "POSTO AGRICOPEL LTDA. - 83488882000456"},
+];
+
+const fechamentos: SelectProps['options'] = [
+  {label: "DEZENAL + 10", value: "DEZENAL + 10"},
+  {label: "DEZENAL + 15", value: "DEZENAL + 15"},
+  {label: "DEZENAL + 20", value: "DEZENAL + 20"},
+  {label: "DEZENAL + 45", value: "DEZENAL + 45"},
+  {label: "SEMANAL + 5", value: "SEMANAL + 5"},
+  {label: "SEMANAL + 7", value: "SEMANAL + 7"},
+  {label: "DIÁRIO + 3", value: "DIÁRIO + 3"},
+  {label: "DIÁRIO + 5", value: "DIÁRIO + 5"},
+];
+
+const movimentos: SelectProps['options'] = [
+  {label: "VENDA", value: "VENDA"},
+  {label: "SAQUE", value: "SAQUE"},
+  {label: "PIX", value: "PIX"},
+  {label: "CONTESTAÇÃO", value: "CONTESTAÇÃO"},
 ];
 
 // for (let i = 10; i < 36; i++) {
@@ -27,7 +52,32 @@ const handleChange = (value: string) => {
 };
 
 const App: React.FC = () => (
-  [<Select
+  [
+    <DatePicker 
+      onChange={onChange}
+      placeholder="Emissão de.."  
+      style={{ width: '50%' }}
+    />,
+    <DatePicker
+      onChange={onChange}
+      placeholder="até.."  
+      style={{ width: '50%' }}
+    />,
+    <Select
+    mode="tags"
+    style={{ width: '50%' }}
+    placeholder="Tipo movimento"
+    onChange={handleChange}
+    options={movimentos}
+  />,
+  <Select
+    mode="tags"
+    style={{ width: '50%' }}
+    placeholder="Tipo Fechamento"
+    onChange={handleChange}
+    options={fechamentos}
+  />,
+  <Select
     mode="tags"
     style={{ width: '50%' }}
     placeholder="Incluir cliente"
@@ -54,7 +104,14 @@ const App: React.FC = () => (
     placeholder="Remover Fornecedor"
     onChange={handleChange}
     options={fornecedores}
-  />,]
+  />,
+  <Select
+    mode="tags"
+    style={{ width: '50%' }}
+    placeholder="Remover Movimentos código.."
+    onChange={handleChange}
+  />,
+  ]
 );
 
 export default App;
